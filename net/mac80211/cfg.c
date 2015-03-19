a484f3b7fb1e3aff9b2cd5f665d437bc25cdf858
Eliad Peller <eliad@wizery.com>
2015-03-19 17:20:32 +0200

    mac80211: allow CSA while scanning/ROC (INTERNAL)
    Some devices are smart-enough to manage CSA timing
    while scanning/ROC. let them handle it.
    
    TODO: add feature flag, etc.
    
    Signed-off-by: Eliad Peller <eliad@wizery.com>

--- net/mac80211/cfg.c
+++ net/mac80211/cfg.c
@@ -3204,9 +3204,6 @@ __ieee80211_channel_switch(struct wiphy *wiphy, struct net_device *dev,
 	sdata_assert_lock(sdata);
 	lockdep_assert_held(&local->mtx);
 
-	if (!list_empty(&local->roc_list) || local->scanning)
-		return -EBUSY;
-
 	if (sdata->wdev.cac_started)
 		return -EBUSY;
 
